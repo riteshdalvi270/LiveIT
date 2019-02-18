5e0cf462a16d902fc3913d80b518e4df2b4a1433
Ritesh Dalvi <riteshdalvi270@gmail.com>
2019-02-17 22:50:56 -0800

    Added SpringBoot application setup

--- liveIt-service/src/main/java/com/liveit/LiveITApplication.java
+++ liveIt-service/src/main/java/com/liveit/LiveITApplication.java
@@ -1,7 +1,13 @@
 package com.liveit;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiveITApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveITApplication.class, args);
+    }
+
 }
