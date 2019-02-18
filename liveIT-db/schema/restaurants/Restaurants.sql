8843ceb244c832c29d4524a1e2e094fb634c9fcf
ridonkz <ridima123@gmai.com>
2019-02-17 21:31:37 -0800

    Adding tables for restaurants

---
+++ liveIT-db/schema/restaurants/Restaurants.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Restaurants (
+id INT AUTO_INCREMENT,
+name VARCHAR(200) NOT NULL,
+address VARCHAR(250) NOT NULL,
+visitedDate DATETIME,
+webURL VARCHAR(500),
+yelpURL VARCHAR(500),
+cuisineId INT NOT NULL,
+ourRating INT NOT NULL,
+expense  VARCHAR(4),
+amtPaid DECIMAL(5,2) NOT NULL,
+isClosed BOOLEAN NOT NULL,
+versionStartDate DATE DEFAULT now(),
+versionEndDate DATE,
+venueId INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(cuisineId) REFERENCES cuisine(id),
+FOREIGN KEY(venueId) REFERENCES venue(id)
+)ENGINE=INNODB;
